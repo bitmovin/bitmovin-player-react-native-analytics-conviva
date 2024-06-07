f03e235b4c7f04169304a0290188740626ef5644
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2024-06-07 15:48:38 +0200

    remove unneeded helper

--- ios/RCTBridge+Generics.swift
+++
@@ -1,8 +0,0 @@
-import React
-
-extension RCTBridge {
-    /// Helper to allow acquiring typed module instance from the bridge.
-    subscript<T: RCTBridgeModule>(_ moduleType: T.Type) -> T? {
-        module(for: moduleType) as? T
-    }
-}
